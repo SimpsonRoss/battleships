7c520a707efff5cdecc0a8dcb13725faa39bfbe7
SimpsonRoss <thisisrosssimpson@gmail.com>
2023-06-23 08:18:49 -0400

    Randomly place the ships on the grid, checking the space is available. Hide the ships of the computer.

--- js/script.js
+++ js/script.js
@@ -1,39 +1,54 @@
 /*----- constants -----*/
+
+//Call AUDIO.play to trigger this -> const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
 const ROWS = 6;
 const COLS = 10;
 
-const COLORS = {
+// An object to store the color values for the player board
+const PLAYERCOLORS = {
   '-2': 'green',
   '-1': 'red',
   '0': 'lightgrey',
   '1': 'grey',
 }
+// An object to store the color values for the computer board, which hides the computer ships
+const COMPCOLORS = {
+  '-2': 'green',
+  '-1': 'red',
+  '0': 'lightgrey',
+  '1': 'lightgrey',
+}
 
+// An object to store the player names, incase I want to adjust these
+const TURNS = {
+  player: 'Humanity',
+  computer: 'Alien Race',
+}
 
 /*----- state variables -----*/
 let computerBoardArr;
 let playerBoardArr;
 let computerFleet;
 let playerFleet;
-let turn;
-let winner;
+let turn; // 1 = Players Turn & -1 = Computers Turn 
+let winner; // null = game in play, 1 = Player Wins, -1 = Computer Wins
 
 
 /*----- cached elements  -----*/
 const computerBoard = document.querySelector('#computerBoard');
 const playerBoard = document.querySelector('#playerBoard');
-
+const messageEl = document.querySelector('#messageBar');
 
 
 /*----- event listeners -----*/
 computerBoard.addEventListener('click', boardClick);
-//playerBoard.addEventListener('click', boardClick);
-
+playerBoard.addEventListener('click', boardClick);
 
 
 /*----- functions -----*/
 
-const init = () => {
+function init() {
+
   computerBoardArr = [
     [0, 0, 0, 0, 0, 0], //col 0
     [0, 0, 0, 0, 0, 0], //col 1
@@ -73,50 +88,53 @@ const init = () => {
     [1, 1],          //sm-ship
   ];
 
-  generateBoard(computerBoard);
-  generateBoard(playerBoard);
+  generateBoard(computerBoard, false);
+  generateBoard(playerBoard, true);
+  placeShipsOnBoard(playerFleet, playerBoardArr)
+  placeShipsOnBoard(computerFleet, computerBoardArr)
   turn = 1;
   winner = null;
   render();
 }
 
+
 function boardClick(evt) {
   // Guards:
-  // returns without procesing, if someone has won already or it's a tie
+  // returns without processing, if someone has won already or it's a tie
   if (winner !== null) {
     return;
   }
-  // returns if the user clicks on something that isn't one of our 9 divs (a.k.a. the playable board)
-  if (!evt.target.id.startsWith('c')) {
-    return;
-  }
 
-  // Split the id string of the div to get the index
-  const idOfSquare = evt.target.id.split('c')[1].split('r');
-  console.log("click");
-  console.log(idOfSquare);
+  const idOfSquare = evt.target.id.slice(2).split('r');
   const colIdx = parseInt(idOfSquare[0]);
   const rowIdx = parseInt(idOfSquare[1]);
 
-  //changing the value of the bombed square depending on it's original state
-  computerBoardArr[colIdx][rowIdx] = -2;
-  // ^^^^ Temp assigning this as -3 until I can figure out the algorithm
+  // On player's turn, interact with computer's board
+  if (turn === 1 && evt.target.id.startsWith('C')) {
+    computerBoardArr[colIdx][rowIdx] -= 2;
+    turn *= -1; // Switch to computer's turn
+  }
 
-  // Switch player turn
-  turn *= -1;
+  // On computer's turn, interact with player's board
+  if (turn === -1 && evt.target.id.startsWith('P')) {
+    playerBoardArr[colIdx][rowIdx] -= 2;
+    turn *= -1; // Switch to player's turn
+  }
+
   //Check for winner
-  //winner = getWinner();
+  winner = getWinner();
   render();
 }
 
 
-const generateBoard = (board, rows = ROWS, columns = COLS) => {
-  for (let row = 0; row < rows; row++) {
 
+function generateBoard(board, isPlayerBoard, rows = ROWS, columns = COLS) {
+  for (let row = 0; row < rows; row++) {
     for (let column = 0; column < columns; column++) {
       const elColumn = document.createElement('div');
 
-      let columnId = `c${String(column)}`;
+      let prefix = isPlayerBoard ? 'P' : 'C';
+      let columnId = `${prefix}c${String(column)}`;
       let rowId = `r${String(row)}`;
       elColumn.id = `${columnId}${rowId}`;
 
@@ -125,21 +143,118 @@ const generateBoard = (board, rows = ROWS, columns = COLS) => {
   }
 }
 
-const render = () => {
-  //renderSquares();
-  //renderMessage();
-  renderBoard();
-
+function render() {
+  renderMessage();
+  renderBoard(computerBoardArr, false, COMPCOLORS);
+  renderBoard(playerBoardArr, true, PLAYERCOLORS);
 }
 
-function renderBoard() {
-  computerBoardArr.forEach(function (colArr, colIdx) {
+
+function renderBoard(boardArr, isPlayerBoard, Colors) {
+  // I'm using the prefixes P = player C = computer to amend the ids for each cell div. 
+  const prefix = isPlayerBoard ? 'P' : 'C';
+  boardArr.forEach(function (colArr, colIdx) {
     colArr.forEach(function (cellVal, rowIdx) {
-      const cellId = `c${colIdx}r${rowIdx}`;
+      const cellId = `${prefix}c${colIdx}r${rowIdx}`;
       const cellEl = document.querySelector(`#${cellId}`);
-      cellEl.style.backgroundColor = `${COLORS[cellVal]}`;
+      cellEl.style.backgroundColor = `${Colors[cellVal]}`;
     })
   });
 }
+
+
+function getWinner() {
+  // flattens the whole array and then checks if every cell is zero or less 
+  // i.e. the board owner has no ships and has lost 
+  if (computerBoardArr.flat().every(cell => cell <= 0)) {
+    console.log('humans win');
+    return 1;
+  } else if (playerBoardArr.flat().every(cell => cell <= 0)) {
+    console.log('aliens win');
+    return -1;
+  }
+  // If we haven't returned yet, there's no winner
+  console.log('game is still in play');
+  return null;
+}
+
+function renderMessage() {
+  if (winner !== null) {
+    messageEl.innerHTML = `${winner === 1 ? TURNS.player : TURNS.computer} Wins!`;
+    //scoreBoard.innerHTML = `<strong>SCORES: ${player1}: ${player1Score} | ${player2}: ${player2Score}</strong>`;
+    //else, the game is in play
+  } else {
+    messageEl.innerHTML = `${turn === 1 ? TURNS.player : TURNS.computer}'s Turn`;
+    //scoreBoard.innerHTML = `<strong>SCORES: ${player1}: ${player1Score} | ${player2}: ${player2Score}</strong>`;
+  }
+};
+
+// a function to make sure the ship can be place within the confines of the board dimensions and not overlap other ships
+function canPlaceShip(board, ship, startRow, startCol, direction) {
+  const length = ship.length;
+  // checks if the ship is horizontal and if it's length from the starting column will exceed the board length
+  if (direction === 'h') {
+    if (startCol + length > board[0].length) {
+      return false;
+    }
+    // if it fits, then we check if any of the squares it will occupy aren't 0 i.e. aren't empty
+    for (let i = 0; i < length; i++) {
+      if (board[startCol + i][startRow] !== 0) {
+        return false;
+      }
+    }
+    // if the direction is vertical then we make the same checks but in the vertical axis
+  } else {
+    if (startRow + length > board.length) {
+      return false;
+    }
+    for (let i = 0; i < length; i++) {
+      if (board[startCol][startRow + i] !== 0) {
+        return false;
+      }
+    }
+  }
+  // returns true, to let placeShipsOnBoard that we're good to go and it's clear to place
+  return true;
+}
+
+// our ship placing function that can amend the board arrays to add the fleet
+function placeShip(board, ship, startRow, startCol, direction) {
+  const length = ship.length;
+  // placing a ship horizontally
+  if (direction === 'h') {
+    for (let i = 0; i < length; i++) {
+      board[startCol + i][startRow] = ship[i];
+    }
+  } else {
+    for (let i = 0; i < length; i++) {
+      board[startCol][startRow + i] = ship[i];
+    }
+  }
+}
+
+// iterates through the fleet with a forEach, generates a random location and direction
+// calls canPlaceShip to check it's safe, and if so calls placeShip 
+function placeShipsOnBoard(fleet, board) {
+  fleet.forEach(ship => {
+    let placed = false;
+    while (!placed) {
+      // finding a random number within the length of the row
+      const startRow = Math.floor(Math.random() * board[0].length);
+      // finding a random number within the length of the column
+      const startCol = Math.floor(Math.random() * board.length);
+      // choosing horizontal or vertical via the size of random decimal
+      const direction = Math.random() < 0.5 ? 'h' : 'v';
+
+      if (canPlaceShip(board, ship, startRow, startCol, direction)) {
+        placeShip(board, ship, startRow, startCol, direction);
+        //ending the loop
+        placed = true;
+      }
+    }
+  });
+}
+
+
 
 init();
